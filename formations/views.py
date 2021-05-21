c8ce202d4db3c422ad19621d21a699aefc04933e
jeddou mhammed <jeddou.ehtp@gmail.com>
2021-05-21 12:10:54 +0100

    add count function to enumerate object

--- formations/views.py
+++ formations/views.py
@@ -15,14 +15,26 @@ def training_course(request,slug) :
 
 
     return render(request, 'formations/training_course.html', {'training_courses' : user.course.all()})
-
+import pickle
 def coure_detail(request,slug) :
     #obj = Course.objects.get(slug=slug1)
     #print(obj.training.slug ,"***********************************")
     # return redirect('coure_detail-path' , slug = obj.slug)
-    print(enumerate(Training.objects.all(),1),"------------------------")
-    return render(request,'formations/course_detail.html',{'course' :Course.objects.get(slug=slug),
-                                                           
-                                                           'training' :enumerate(Training.objects.all(),1)
-                                                           
-                                                           })
+    # print(Course.objects.get(slug=slug).training.slug,"+++++++++++++++++++++++++")
+    # slg = Course.objects.all(slug=slug).training.slug
+    qs = Training.objects.all()
+
+    for i in qs :
+        i=i.course.all()
+        
+        print(i,i.count(),"iiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+        
+
+
+
+    return render(request,'formations/course_detail.html',{'course1' :Course.objects.get(slug=slug),
+                                                        
+                                                        'training' :enumerate(Training.objects.all(),1),
+                        #   'qs' : qs  ,
+                            
+                                                        })
